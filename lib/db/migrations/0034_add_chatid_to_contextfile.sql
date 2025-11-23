170ae36f28adc5daad7c26f94f263566f696a7ac
Hilton Brown <hello@hiltonbrown.com.au>
2025-11-23 17:29:21 +1000

    Clean up

--- lib/db/migrations/0034_add_chatid_to_contextfile.sql
+++
@@ -1,6 +0,0 @@
--- Add chatId column to ContextFile table to track which chat the file originated from
-ALTER TABLE "ContextFile" ADD COLUMN "chatId" uuid;
-
--- Add foreign key constraint with SET NULL on delete
-ALTER TABLE "ContextFile" ADD CONSTRAINT "ContextFile_chatId_Chat_id_fk"
-  FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE set null ON UPDATE no action;
