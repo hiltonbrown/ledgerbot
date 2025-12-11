70e95fa4195661f8065e3a55611a8dd5e547e319
Hilton <hiltonbrown@users.noreply.github.com>
2025-12-11 20:48:11 +1000

    Align Clerk profile styling with theme

--- app/account/[[...rest]]/page.tsx
+++ app/account/[[...rest]]/page.tsx
@@ -2,10 +2,12 @@
 
 import { UserProfile } from "@clerk/nextjs";
 
+import { clerkAppearance } from "@/lib/clerk/appearance";
+
 export default function AccountPage() {
   return (
     <div className="flex h-full w-full items-center justify-center bg-background">
-      <UserProfile />
+      <UserProfile appearance={clerkAppearance} />
     </div>
   );
 }
